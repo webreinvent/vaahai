bf6ac69b88fa5c3cb98efc837f29d84a18ec09a5
Pradeep Kumar <webreinvent@gmail.com>
2025-06-06 17:35:01 +0530

    backup

--- vaahai/cli/commands/dev/__init__.py
+++ vaahai/cli/commands/dev/__init__.py
@@ -7,7 +7,7 @@ This module contains commands for development and testing purposes.
 import typer
 from vaahai.cli.commands.helloworld.command import helloworld_app
 from vaahai.cli.commands.dev.showcase import showcase_app
-from vaahai.cli.commands.dev.prompt_showcase import prompt_showcase_app
+from vaahai.cli.commands.dev.prompts import prompts_app
 
 # Create a Typer instance for the dev command group
 dev_app = typer.Typer(help="Development and testing commands")
@@ -15,4 +15,4 @@ dev_app = typer.Typer(help="Development and testing commands")
 # Register subcommands
 dev_app.add_typer(helloworld_app, name="helloworld")
 dev_app.add_typer(showcase_app, name="showcase")
-dev_app.add_typer(prompt_showcase_app, name="prompt_showcase")
+dev_app.add_typer(prompts_app, name="prompts")
